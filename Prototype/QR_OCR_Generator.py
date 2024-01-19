c4d09dad3937261e86c994ed1ca54bd31b37cb77
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-01-19 20:12:07 +0530

    Fix spacing

--- Prototype/QR_OCR_Generator.py
+++ Prototype/QR_OCR_Generator.py
@@ -30,7 +30,7 @@ def save_qr_code(employee_id, folder='QR_ID'):
     # Create image with caption
     img_with_caption = Image.new(
         'RGB', (qr_img.size[0], qr_img.size[1] + 50), color='white')
-    img_with_caption.paste(qr_img, (0, 20))  # Move QR code down by 10 pixels
+    img_with_caption.paste(qr_img, (0, 20))  # Move QR code down by 20 pixels
 
     # Add caption text
     draw = ImageDraw.Draw(img_with_caption)
@@ -48,13 +48,13 @@ def save_qr_code(employee_id, folder='QR_ID'):
     img_with_borders = ImageOps.expand(
         img_with_borders, border=spacing, fill='white')
 
-    # Add employee logo to the top
+    # Offset the employee logo by 5 pixels from the top
     logo_path = "./static/images/emp_logo.png"
     if os.path.exists(logo_path):
         logo = Image.open(logo_path)
         logo = logo.resize((50, 50))  # Adjust the size as needed
         img_with_borders.paste(
-            logo, ((img_with_borders.width - logo.width) // 2, spacing))
+            logo, ((img_with_borders.width - logo.width) // 2, spacing + 7))
 
     # Save image
     image_path = f"{folder}/{employee_id}.png"
