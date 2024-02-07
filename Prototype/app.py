424ab5486f4088195025ec46b87fdaa2b96c9d68
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-02-07 12:50:02 +0530

    Setup mongodb

--- Prototype/app.py
+++ Prototype/app.py
@@ -1,10 +1,58 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
+
+
+# Testing Parameters
+TEST_CONNECTION = True     # ? True/False: Test/Don't test the connection to MongoDB
+
+
+# Define db and collection as placeholders
+db = None
+collection = None
+
+# MongoDB connection
+mongodb_uri = os.getenv("MONGODB_URI")
+client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+
+# Select the database and collection
+db = client['DBMS_db']
+collection = db['qr_codes']
+
+# Send a ping to confirm a successful connection
+if TEST_CONNECTION:
+    try:
+        client.admin.command('ping')
+        print("✅ Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    # Add a sample document in the collection (don't add if already exists)
+    name = 'John Doe'
+    if collection.count_documents({'name': name}) == 0:
+        collection.insert_one({'name': name})
+        print("✅ Added a sample document in the collection.")
+
+    # Retrieve the sample document added
+    result = collection.find_one({'name': name})
+    print(f"✅ Retrieved the sample document: {result}")
+
+    # Delete the sample document added
+    collection.delete_one({'name': name})
+    print("✅ Deleted the sample document.")
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
