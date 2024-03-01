b3c692b115c04540ac7a0029c99cc7b12dbf60cf
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2024-03-01 11:59:30 +0530

    Add mongodb connection test

--- Prototype/app.py
+++ Prototype/app.py
@@ -3,6 +3,12 @@ from QR_OCR_Generator import generate_customer_id
 from flask_mysqldb import MySQL
 from datetime import date
 import random
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -18,6 +24,48 @@ customer_id = "none"
 name = "none"
 
 mysql = MySQL(app)
+
+
+# Testing Parameters
+TEST_CONNECTION = True  # ? True/False: Test/Don't test the connection to MongoDB
+
+# Define mongo_db and collection as placeholders
+mongo_db = None
+mongo_collection = None
+
+# MongoDB connection
+mongodb_uri = os.getenv("MONGODB_URI")
+client = MongoClient(mongodb_uri, server_api=ServerApi('1'))
+
+# Select the database and collection
+mongo_db = client['DBMS_db']
+mongo_collection = mongo_db['qr_codes']
+
+# Send a ping to confirm a successful connection
+if TEST_CONNECTION:
+    print("=== Testing MongoDB Connection ===")
+
+    try:
+        client.admin.command('ping')
+        print("✅ Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+    # Add a sample document in the collection (don't add if already exists)
+    name = 'John Doe'
+    if mongo_collection.count_documents({'name': name}) == 0:
+        mongo_collection.insert_one({'name': name})
+        print("✅ Added a sample document in the collection.")
+
+    # Retrieve the sample document added
+    result = mongo_collection.find_one({'name': name})
+    print(f"✅ Retrieved the sample document: {result}")
+
+    # Delete the sample document added
+    mongo_collection.delete_one({'name': name})
+    print("✅ Deleted the sample document.")
+
+    print("=== MongoDB Connection Test Completed ===")
 
 
 @app.route('/', methods=['GET', 'POST'])
